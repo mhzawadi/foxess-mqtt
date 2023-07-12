12c76333c272b27e2fbd77a9603b2c60dee95126
Matthew Horwood <matt@horwood.biz>
2023-07-12 12:43:24 +0100

    add PHP timezone debug

--- run.php
+++ run.php
@@ -4,4 +4,5 @@ namespace MHorwood\loader;
 use MHorwood\foxess_mqtt\controller\foxess_data;
 require __DIR__ . '/vendor/autoload.php';
 
+echo "This is our timezone: ".timezone_location_get();
 $foxess = new foxess_data();
