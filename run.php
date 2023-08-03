2c3f6c62453df64fe8c9302e8f41d6a42829e86c
Matthew Horwood <matt@horwood.biz>
2023-08-03 09:57:19 +0100

    bug: add new line

--- run.php
+++ run.php
@@ -4,5 +4,5 @@ namespace MHorwood\loader;
 use MHorwood\foxess_mqtt\controller\foxess_data;
 require __DIR__ . '/vendor/autoload.php';
 
-echo "This is our timezone: ".date_default_timezone_get();
+echo "This is our timezone: ".date_default_timezone_get()."\n";
 $foxess = new foxess_data();
