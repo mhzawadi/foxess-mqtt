df748d5bbd0a394b49924e5e1588d9eddda87bbb
Matthew Horwood <matt@horwood.biz>
2024-06-05 07:59:42 +0100

    Alpine 3.20
    feat: update to Alpine 3.20
    chor: tidy up docker files

--- shell/test.sh
+++ shell/test.sh
@@ -9,7 +9,7 @@ then
 elif [ "$1" == "up" ]
 then
   docker image rm mhzawadi/foxess-mqtt:dev-php && \
-  docker build -t mhzawadi/foxess-mqtt:dev-php -f Dockerfile-php . && \
+  docker build -t mhzawadi/foxess-mqtt:dev-php -f Dockerfile . && \
   docker run --rm -t -v "${PWD}":/workdir overtrue/phplint:latest ./ --exclude=vendor --no-configuration --no-cache && \
   docker compose -f docker-compose-dev.yml up -d
 else
