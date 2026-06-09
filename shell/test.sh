4599704f3eac71bad1200b36ecee22ab399a4b36
Matthew Horwood <matt@horwood.biz>
2026-06-09 19:58:33 +0100

    package update

--- shell/test.sh
+++ shell/test.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-if [ $# -lt 1 ]
+if [ "$1" = "test" ]
 then
   docker scout quickview fs://.
   docker scout cves fs://.
   docker run --rm -t -v "${PWD}":/workdir overtrue/phplint:latest ./ --exclude=vendor --no-configuration --no-cache && \
   docker compose -f docker-compose-dev.yml up foxess-mqtt
-elif [ "$1" == "composer" ]
+elif [ "$1" = "composer" ]
 then
   docker image rm mhzawadi/foxess-mqtt:dev-php;
   docker build -t mhzawadi/foxess-mqtt:dev-php -f Dockerfile . && \
   docker run --rm -it -v '/Users/matt/git/foxess-mqtt:/foxess-mqtt' mhzawadi/foxess-mqtt:dev-php /usr/local/bin/composer upgrade
-elif [ "$1" == "up" ]
+elif [ "$1" = "up" ]
 then
   docker image rm mhzawadi/foxess-mqtt:dev-php && \
   docker build -t mhzawadi/foxess-mqtt:dev-php -f Dockerfile . && \
